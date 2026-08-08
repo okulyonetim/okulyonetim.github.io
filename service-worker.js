3e68fed6ec8c9066eee646300321f975fb8650f9
sedonet23 <sedonet23@gmail.com>
2026-08-08 19:43:55 +0300

    Increment cache version to v383
    Updated cache name to version 383.

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    · Strateji: statik dosyalar "Cache First", dış kaynaklar "Network First"
    ==================================================================== */
 
-const CACHE_ADI = 'oy-cache-v382';
+const CACHE_ADI = 'oy-cache-v383';
 
 /* ---- Önbelleğe alınacak tüm uygulama dosyaları ---- */
 const ONBELLEGE_ALINACAKLAR = [
