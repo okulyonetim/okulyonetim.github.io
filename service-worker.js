8de0eab1ce250b5c2adf7943b1108df95bcbd61b
sedonet23 <sedonet23@gmail.com>
2026-08-08 14:20:28 +0300

    Update cache name to 'oy-cache-v379'

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    · Strateji: statik dosyalar "Cache First", dış kaynaklar "Network First"
    ==================================================================== */
 
-const CACHE_ADI = 'oy-cache-v378';
+const CACHE_ADI = 'oy-cache-v379';
 
 /* ---- Önbelleğe alınacak tüm uygulama dosyaları ---- */
 const ONBELLEGE_ALINACAKLAR = [
