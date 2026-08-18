f3724281d0884c32e0ac4667459626f12d89fa0c
sedonet23 <sedonet23@gmail.com>
2026-08-18 13:07:13 +0300

    Özel Menü rol güvenliği testlerini ekle

---
+++ tests/ozel-menu-role-rules.test.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const {
+  initializeTestEnvironment,
+  assertSucceeds,
+  assertFails,
+} = require('@firebase/rules-unit-testing');
+const { doc, setDoc, getDoc, updateDoc, deleteDoc } = require('firebase/firestore');
+
+const PROJECT_ID = 'demo-okul-rules';
+
+async function main(){
+  const rules = fs.readFileSync('firestore.rules','utf8');
+  const testEnv = await initializeTestEnvironment({ projectId:PROJECT_ID, firestore:{rules} });
+
+  try {
+    await testEnv.clearFirestore();
+    await testEnv.withSecurityRulesDisabled(async context => {
+      const db = context.firestore();
+      await setDoc(doc(db,'oy_roller','rol-editor'),{yetkiler:{ozelMenu:'duzenle'}});
+      await setDoc(doc(db,'oy_roller','rol-viewer'),{yetkiler:{ozelMenu:'goruntule'}});
+      await setDoc(doc(db,'oy_kullanicilar','editorUid'),{uid:'editorUid',admin:false,aktif:true,rolId:'rol-editor'});
+      await setDoc(doc(db,'oy_kullanicilar','viewerUid'),{uid:'viewerUid',admin:false,aktif:true,rolId:'rol-viewer'});
+      await setDoc(doc(db,'oy_kullanicilar','adminUid'),{uid:'adminUid',admin:true,aktif:true});
+      await setDoc(doc(db,'oy_ozelMenu','g1'),{ad:'Özel Grup',renk:'#123456',sira:1,ogeler:[]});
+    });
+
+    const editorDb = testEnv.authenticatedContext('editorUid').firestore();
+    const viewerDb = testEnv.authenticatedContext('viewerUid').firestore();
+    const adminDb = testEnv.authenticatedContext('adminUid').firestore();
+    const anonDb = testEnv.unauthenticatedContext().firestore();
+
+    await assertSucceeds(getDoc(doc(viewerDb,'oy_ozelMenu','g1')));
+    await assertFails(getDoc(doc(anonDb,'oy_ozelMenu','g1')));
+
+    await assertSucceeds(setDoc(doc(editorDb,'oy_ozelMenu','g2'),{ad:'Yeni Grup',ogeler:[]}));
+    await assertSucceeds(updateDoc(doc(editorDb,'oy_ozelMenu','g1'),{ad:'Güncel Grup'}));
+    await assertFails(updateDoc(doc(viewerDb,'oy_ozelMenu','g1'),{ad:'Yetkisiz'}));
+    await assertFails(deleteDoc(doc(viewerDb,'oy_ozelMenu','g1')));
+    await assertSucceeds(deleteDoc(doc(adminDb,'oy_ozelMenu','g1')));
+
+    console.log('Özel Menü rol güvenliği testleri başarılı.');
+  } finally {
+    await testEnv.cleanup();
+  }
+}
+
+main().catch(err=>{console.error(err);process.exit(1);});
