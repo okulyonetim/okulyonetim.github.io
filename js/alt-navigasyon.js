ccfcca42d2f8edee8405a89f376fd797a1010e4f
sedonet23 <sedonet23@gmail.com>
2026-08-23 00:02:31 +0300

    Load written exam live sync patch

--- js/alt-navigasyon.js
+++ js/alt-navigasyon.js
@@ -38,7 +38,9 @@
       yukle('js/ogretmenler-modern.js?v=2','data-ogretmenler-modern');
       yukle('js/ogretmen-detay-modern.js?v=3','data-ogretmen-detay-modern');
       yukle('js/ayarlar-modern.js?v=2','data-ayarlar-modern');
-      yukle('js/yazili-sinavlar-modern.js?v=4','data-yazili-sinavlar-modern');
+      yukle('js/yazili-sinavlar-modern.js?v=4','data-yazili-sinavlar-modern',function(){
+        yukle('js/yazili-sinav-live-sync.js?v=1','data-yazili-sinav-live-sync');
+      });
       /* Android rapor akışı: gerçek A4 önizleme -> native PrintPlugin */
       yukle('js/native-report-preview.js?v=2','data-native-report-preview',function(){
         yukle('js/yazili-rapor-modern.js?v=1','data-yazili-rapor-modern');
