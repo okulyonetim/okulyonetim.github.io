12c80705f8dc56567e9379acecdd9fbf3a9c9a97
sedonet23 <sedonet23@gmail.com>
2026-08-22 19:20:17 +0300

    Ogretmen detay modern tasarimini yukleme zincirine ekle

--- js/alt-navigasyon.js
+++ js/alt-navigasyon.js
@@ -10,10 +10,12 @@
   function stilYukle(href,attr){if(document.querySelector('link['+attr+']'))return;var l=document.createElement('link');l.rel='stylesheet';l.href=href;l.setAttribute(attr,'1');document.head.appendChild(l);}
   stilYukle('css/alt-navigation-theme.css?v=11','data-alt-nav-theme');
   stilYukle('css/ogretmenler-modern.css?v=2','data-ogretmenler-modern-style');
+  stilYukle('css/ogretmen-detay-modern.css?v=1','data-ogretmen-detay-modern-style');
   yukle('js/alt-navigasyon-core.js','data-alt-nav-core',function(){
     yukle('js/alt-navigation-list-theme.js?v=1','data-alt-nav-list-theme',function(){
       yukle('js/ui-stability-fixes.js','data-ui-stability');
       yukle('js/ogretmenler-modern.js?v=2','data-ogretmenler-modern');
+      yukle('js/ogretmen-detay-modern.js?v=1','data-ogretmen-detay-modern');
     });
   });
 })();
