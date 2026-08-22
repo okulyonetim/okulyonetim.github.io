131d47f6849d4d0d6a491a33bea9a93ffec4804c
sedonet23 <sedonet23@gmail.com>
2026-08-22 12:10:44 +0300

    Logo teması için cache v533

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* ====================================================================
    Okul Yönetim Paneli — Service Worker v7
    ==================================================================== */
-const CACHE_ADI = 'oy-cache-v532';
+const CACHE_ADI = 'oy-cache-v533';
 
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
