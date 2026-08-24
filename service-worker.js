749d739b61bf3700e8f54fa295c4e26db97660a4
sedonet23 <sedonet23@gmail.com>
2026-08-24 21:30:36 +0300

    Kontrol listesi tiklerini ve madde aksiyonlarını düzelt

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Okul Yönetim Paneli — Service Worker v8 / performans */
-const CACHE_ADI='oy-cache-v640';
+const CACHE_ADI='oy-cache-v641';
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 firebase.initializeApp({apiKey:"AIzaSyCxLLlLCA0Deu7dcQch5e1c4R5ur5FSkc",authDomain:"okul-6e302.firebaseapp.com",projectId:"okul-6e302",storageBucket:"okul-6e302.firebasestorage.app",messagingSenderId:"738103486583",appId:"1:738103486583:web:da91129b1a08f2463efe72"});
