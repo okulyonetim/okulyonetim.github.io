d1a8a8fe0d639d748b4893440f128683e0c25546
sedonet23 <sedonet23@gmail.com>
2026-08-25 13:10:35 +0300

    refactor(people): remove obsolete yoklama global bridge

--- js/yoklama-global-bridge.js
+++
@@ -1,6 +0,0 @@
-/* Yoklama service/repository klasik script global köprüsü */
-(function(){
-'use strict';
-try{if(typeof YoklamaRepository!=='undefined')window.YoklamaRepository=YoklamaRepository}catch(e){console.warn('[yoklama] repository bridge',e)}
-try{if(typeof YoklamaService!=='undefined')window.YoklamaService=YoklamaService}catch(e){console.warn('[yoklama] service bridge',e)}
-})();
