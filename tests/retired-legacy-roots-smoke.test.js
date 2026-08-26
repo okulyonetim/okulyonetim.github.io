4e38b68b964c0add1372a9f6f4c4cd2d299c4523
sedonet23 <sedonet23@gmail.com>
2026-08-26 05:14:36 +0300

    test(widget): lock retired wrapper

--- tests/retired-legacy-roots-smoke.test.js
+++ tests/retired-legacy-roots-smoke.test.js
@@ -13,6 +13,7 @@ const retired=[
   'js/ozellik-katalogu.js',
   'js/pdf-export-libs.js',
   'js/ogretmen-liste-olusturucu.js',
+  'js/widget-plugin.js',
   'js/modules/transport-data.js',
   'js/modules/duty-data.js',
   'js/modules/settings-data.js'
