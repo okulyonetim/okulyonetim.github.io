cfd5a3bee1af0d776a3730dca05a10f506c3a984
sedonet23 <sedonet23@gmail.com>
2026-08-26 05:19:54 +0300

    test(architecture): lock root javascript inventory

---
+++ tests/root-js-inventory-smoke.test.js
@@ -0,0 +1,28 @@
+const fs=require('fs');
+const assert=require('assert');
+
+const allowed=[
+  'app-loader.js',
+  'asistan.js',
+  'auth.js',
+  'dokuman-okuyucu.js',
+  'firebase-init.js',
+  'harita.js',
+  'kriter-dagitim.js',
+  'maas-degisiklik.js',
+  'map-libs.js',
+  'mevzuat-asistan.js',
+  'proje-degerlendirme.js',
+  'teblig-tebellug.js',
+  'widget-bridge.js'
+].sort();
+
+const actual=fs.readdirSync('js',{withFileTypes:true})
+  .filter(x=>x.isFile()&&x.name.endsWith('.js'))
+  .map(x=>x.name)
+  .sort();
+
+assert.deepStrictEqual(actual,allowed,
+  'js/ kökünde yeni/eksik dosya var. Yeni işlevler mümkünse js/modules veya js/core altında yaşamalı; kök envanter değişikliği bilinçli yapılmalı.');
+
+console.log(`Kök JS envanteri başarılı (${actual.length} dosya).`);
