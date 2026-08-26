82ff4253a897275eaa1ba6d660acc9efc7e94ed4
sedonet23 <sedonet23@gmail.com>
2026-08-26 05:17:32 +0300

    test(documents): lock retired xlsm viewer patch

--- tests/retired-legacy-roots-smoke.test.js
+++ tests/retired-legacy-roots-smoke.test.js
@@ -16,6 +16,7 @@ const retired=[
   'js/widget-plugin.js',
   'js/yedekleme.js',
   'js/yillik-plan-tohum-veri.js',
+  'js/xlsm-viewer-support.js',
   'js/modules/transport-data.js',
   'js/modules/duty-data.js',
   'js/modules/settings-data.js'
