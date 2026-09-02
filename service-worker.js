850add3dbc7eb2b2601417a2bd6bae3b3e988f9d
sedonet23 <sedonet23@gmail.com>
2026-09-02 14:49:01 +0000

    feat: restore attendance grid parity

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Görev: uygulama kabuğunu önbelleğe almak, statik kaynakları çevrimiçiyken
    güncel ağ sürümünden, çevrimdışıyken cache'den sunmak ve Firebase Messaging
    bildirimlerini taşımak. HTML/CSS/JS enjeksiyonu YOK. */
-const CACHE_ADI='oy-cache-v845';
+const CACHE_ADI='oy-cache-v846';
 
 let messaging=null;
 try{
