96a7ebedf48cbe41f191020b0ba6e5ffbdbaf74f
sedonet23 <sedonet23@gmail.com>
2026-09-02 05:47:00 +0300

    fix: refresh shell cache for profile and search updates

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Görev: uygulama kabuğunu önbelleğe almak, statik kaynakları çevrimiçiyken
    güncel ağ sürümünden, çevrimdışıyken cache'den sunmak ve Firebase Messaging
    bildirimlerini taşımak. HTML/CSS/JS enjeksiyonu YOK. */
-const CACHE_ADI='oy-cache-v838';
+const CACHE_ADI='oy-cache-v839';
 
 let messaging=null;
 try{
