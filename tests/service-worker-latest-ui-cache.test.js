75a795e6e09a3658a22e8eadd7f3e186bfc4a3aa
sedonet23 <sedonet23@gmail.com>
2026-09-14 23:18:53 +0300

    test: güncel arayüz dosyalarının yeni cache sürümünde olduğunu doğrula

---
+++ tests/service-worker-latest-ui-cache.test.js
@@ -0,0 +1,8 @@
+const fs=require('fs');
+const assert=require('assert');
+const sw=fs.readFileSync('service-worker.js','utf8');
+new Function(sw);
+assert(sw.includes("const CACHE_ADI='oy-cache-v951'"),'Service Worker cache sürümü güncellenmeli.');
+assert(sw.includes("./js/core/admin-password-reset.js?v=948"),'Şifre sıfırlama köprüsü önbelleğe alınmalı.');
+assert(sw.includes("./js/core/settings-back-navigation.js?v=944"),'Ayarlar geri navigasyon düzeltmesi önbelleğe alınmalı.');
+console.log('Service Worker güncel arayüz cache sözleşmesi başarılı.');
