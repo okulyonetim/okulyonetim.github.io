fb6e388a32a14df9d88b9423f4f51ee95b5fce01
sedonet23 <sedonet23@gmail.com>
2026-09-20 08:27:37 +0300

    chore: refresh schedule report runtime version

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -233,7 +233,7 @@ function firebaseyiBaslat(){
 (function scheduleReportRedesignFeatureLoad(){
   if(document.querySelector('script[data-schedule-report-redesign]'))return;
   const script=document.createElement('script');
-  script.src='js/core/schedule-report-redesign.js?v=941';
+  script.src='js/core/schedule-report-redesign.js?v=942';
   script.async=false;
   script.dataset.scheduleReportRedesign='';
   document.head.appendChild(script);
