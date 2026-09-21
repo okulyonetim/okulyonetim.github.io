681781b8c8328f26b34768032bb6e31ef00c8944
sedonet23 <sedonet23@gmail.com>
2026-09-21 16:52:18 +0300

    fix: servis plan geri yukleme cache v918

--- js/app-loader.js
+++ js/app-loader.js
@@ -26,7 +26,7 @@ define('people',['js/modules/people.js','js/modules/people-import.js','js/module
 define('academic',[FIREBASE_STORAGE_SDK,'js/modules/report-engine.js','js/modules/academic.js?v=882']);
 define('management',['js/modules/report-engine.js','js/modules/management.js?v=882']);
 define('communication',['js/modules/communication.js?v=839','js/modules/assistant.js']);
-define('transport',['js/modules/report-engine.js','js/modules/transport.js?v=917']);
+define('transport',['js/modules/report-engine.js','js/modules/transport.js?v=918']);
 define('documents',[FIREBASE_STORAGE_SDK,'js/modules/report-engine.js','js/modules/documents.js']);
 define('reports',['js/modules/report-engine.js','js/modules/reports.js']);
 define('tools',['js/modules/tools.js','js/modules/rubric-settings.js','js/modules/rubric-tools.js']);
