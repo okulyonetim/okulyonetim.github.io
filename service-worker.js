a8542552674f63c6447c181d985d681d8d9dd089
sedonet23 <sedonet23@gmail.com>
2026-09-23 18:54:54 +0300

    Yemek modülü senkronizasyon düzeltmesini yayınla

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Görev: uygulama kabuğunu önbelleğe almak, uygulama kodunu ve kabuğunu önbellekten hızlıca sunup
    ağı arka planda yenilemek ve Firebase Messaging
    bildirimlerini taşımak. HTML/CSS/JS enjeksiyonu YOK. */
-const CACHE_ADI='oy-cache-v1022';
+const CACHE_ADI='oy-cache-v1023';
 
 let messaging=null;
 try{
