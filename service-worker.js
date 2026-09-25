f3017bb961007a86eb7c1f374c8d9ac14f6593e3
sedonet23 <sedonet23@gmail.com>
2026-09-25 07:25:46 +0300

    Öğretmen tema düzeltmesi için önbelleği yenile

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Görev: uygulama kabuğunu önbelleğe almak, uygulama kodunu ve kabuğunu önbellekten hızlıca sunup
    ağı arka planda yenilemek ve Firebase Messaging
    bildirimlerini taşımak. HTML/CSS/JS enjeksiyonu YOK. */
-const CACHE_ADI='oy-cache-v1059';
+const CACHE_ADI='oy-cache-v1060';
 
 let messaging=null;
 try{
